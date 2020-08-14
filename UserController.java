51642d643e800c3952582e65125f9a74dd44254d
kimjuli62 <kimjuli62@naver.com>
2020-08-14 18:28:47 +0900

    Add UserController[insert]

--- UserController.java
+++ UserController.java
@@ -9,6 +9,20 @@ public class UserController {
 	  return "user/login";
 	}
 
+
+	// 회원추가 페이지로 이동
+	@RequestMapping("/user/insert.do")
+	public String insert() {
+	  logger.info("경로 : insert");
+
+	  return "user/insert";
+	}
+
+
+
+
+
+
 }
 
 
